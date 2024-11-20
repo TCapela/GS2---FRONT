04496d93ac7fa370cf17c5cb2bec22b418c0a481
raphatatto <raphaelatatto@gmail.com>
2024-11-20 04:16:30 -0300

    excluindo profileHeader

--- global-solution/energy-calc/src/components/UserProfile/ProfileHeader.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProfileHeader() {
-    return (
-      <div className="bg-green-200 text-black py-6 px-4">
-        <h2 className="text-2xl font-bold">Bem-vindo, [Nome do Usuário]</h2>
-        <p className="text-lg">Aqui estão suas informações e funcionalidades principais.</p>
-      </div>
-    );
-  }
-  
