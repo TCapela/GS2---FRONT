139883fbcf0f47f316c0138472fdf7e4d4fef2eb
Tcapela <trc2405@gmail.com>
2024-11-21 04:18:32 -0300

    ajuste nas props do cardMebro adicionando o linkedin

--- global-solution/energy-calc/src/components/CardMembro/CardMembro.tsx
+++ global-solution/energy-calc/src/components/CardMembro/CardMembro.tsx
@@ -5,10 +5,11 @@ type CardMembroProps = {
   name: string;
   rm: string;
   githubLink: string;
+  linkedinLink: string;
   image: string;
 };
 
-export default function CardMembro({ name, rm, githubLink, image }: CardMembroProps) {
+export default function CardMembro({ name, rm, githubLink, linkedinLink, image }: CardMembroProps) {
   return (
     <div className="flex flex-col pt-10 sm:pt-4 sm:flex-row items-center justify-around gap-4 relative bg-green-950 text-white p-4 group even:sm:flex-row-reverse rounded-2xl">
       <div className="flex flex-col items-center w-full sm:w-2/4 ">
@@ -25,6 +26,15 @@ export default function CardMembro({ name, rm, githubLink, image }: CardMembroPr
       >
         <FaGithubSquare size={32} />
         <span>Github</span>
+      </a>
+      <a
+        className="flex flex-col items-center justify-center gap-2 w-full sm:w-2/4 transition hover:text-green-600 "
+        href={linkedinLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin size={32} />
+        <span>Linkedin</span>
       </a>
       <Image
         className="absolute -top-16 sm:-right-6 group-even:sm:-left-6 rounded-full w-24 sm:w-32 shadow-md shadow-gray-900 transition hover:scale-105 "
