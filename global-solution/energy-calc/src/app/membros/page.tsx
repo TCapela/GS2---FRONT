e7f4021c8ff4f1e7c3753a361a9d937518672320
Tcapela <trc2405@gmail.com>
2024-11-21 04:16:17 -0300

    adicionando o linkedin na pagina membros

--- global-solution/energy-calc/src/app/membros/page.tsx
+++ global-solution/energy-calc/src/app/membros/page.tsx
@@ -6,18 +6,21 @@ export default function membros() {
       name: "Lucas Rodrigues de Souza",
       rm: "557951",
       githubLink: "https://github.com/Lucasrodrigues0101",
+      linkedinLink:"https://www.linkedin.com/in/lucas-rodrigues-de-souza-75a321265/",
       image: "/imgs/membros/lucas.jpg"
     },
     {
       name: "Raphaela Oliveira Tatto",
       rm: "554983",
       githubLink: "https://github.com/raphatatto",
+      linkedinLink:"https://www.linkedin.com/in/raphaela-oliveira-tatto-7975a2276/",
       image: "/imgs/membros/raphatatto.jpg"
     },
     {
       name: "Tiago Ribeiro Capela",
       rm: "558021",
       githubLink: "https://github.com/TCapela",
+      linkedinLink:"https://www.linkedin.com/in/tiago-capela-897102301/",
       image: "/imgs/membros/capela.jpg"
     },
   ];
